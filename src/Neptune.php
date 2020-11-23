65aa133450608bd2aed47c450c1f34b89e8ae5e3
Rajiv Seelam <rajiv@betalectic.com>
2020-11-23 18:54:15 +0530

    First Commit

---
+++ src/Neptune.php
@@ -0,0 +1,66 @@
+<?php
+namespace Betalectic\Neptune;
+
+use Illuminate\Support\Facades\Http;
+use Log;
+
+class Neptune
+{
+    public $client;
+    public $envrionmentUUID;
+    public $createEvent;
+    public $payload;
+    public $recipients;
+
+    public function __construct($payload, $recipients)
+    {
+        $this->client = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.config('neptune.token')
+        ]);
+
+        $this->envrionmentUUID =  config('neptune.env');
+
+        $this->createEvent =  config('neptune.endpoint')."/api/teams/".config('neptune.team')."/events";
+
+        $this->payload = $payload;
+        $this->recipients = $recipients;
+    }
+
+
+        /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function fire($notificationIdentifier, $identifier = "uuid")
+    {
+        // Log::info("Fire");
+
+        $body = [
+            'environment_uuid' => $this->envrionmentUUID,
+            'payload' => $this->payload,
+            'recipients' => $this->recipients,
+        ];
+
+        if($identifier == 'uuid'){
+            $body['notification_uuid'] = $notificationIdentifier;
+        }
+
+        if($identifier == 'slug'){
+            $body['notification_slug'] = $notificationIdentifier;
+        }
+
+
+        // Log::info($body);
+
+        $response = $this->client->post($this->createEvent, $body);
+
+        $jsonResponse = $response->json();
+
+        // Log::info($jsonResponse);
+
+        return $jsonResponse;
+    }
+
+}
---
+++ src/NeptuneChannel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Betalectic\Neptune;
+
+use Illuminate\Notifications\Notification;
+use Log;
+use Betalectic\Neptune\Neptune;
+
+class NeptuneChannel
+{
+    /**
+     * Send the given notification.
+     *
+     * @param  mixed  $notifiable
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return void
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        $payload = $notification->toNeptune($notifiable);
+
+        $recipients = [
+            [
+                'name' => $notifiable->name, 'email' => $notifiable->email
+            ]
+        ];
+
+        // $payload['name'] = $notifiable->name;
+
+        $neptune = new Neptune($payload, $recipients);
+
+        if(isset($notification->notificationUUID) && $notification->notificationUUID != "" && !is_null($notification->notificationUUID)){
+            $neptune->fire($notification->notificationUUID, 'uuid');
+        }
+
+        
+        if(isset($notification->notificationSlug) && $notification->notificationSlug != "" && !is_null($notification->notificationSlug)){
+            $neptune->fire($notification->notificationSlug, 'slug');
+        }
+
+    }
+}
